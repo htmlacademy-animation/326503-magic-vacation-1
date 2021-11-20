71d03c08e163ef377d523a3e723c4306880f276e
Drakeno <vladislavkoj@gmail.com>
2021-11-20 18:29:54 +0300

    2 icons animation

--- source/js/modules/onload.js
+++ source/js/modules/onload.js
@@ -2,4 +2,8 @@ export default () => {
   document.addEventListener(`DOMContentLoaded`, function () {
     document.body.classList.add(`dom-loaded`);
   });
+
+  const path = document.querySelector(`#letter-7`);
+  const pathLength = path.getTotalLength();
+  console.log(pathLength);
 };
