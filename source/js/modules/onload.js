f9ebdc60e236b1a32f879a26f1cf42ee97e6c704
Drakeno <vladislavkoj@gmail.com>
2021-11-20 22:29:33 +0300

    result3 animation

--- source/js/modules/onload.js
+++ source/js/modules/onload.js
@@ -2,8 +2,4 @@ export default () => {
   document.addEventListener(`DOMContentLoaded`, function () {
     document.body.classList.add(`dom-loaded`);
   });
-
-  const path = document.querySelector(`#letter-7`);
-  const pathLength = path.getTotalLength();
-  console.log(pathLength);
 };
